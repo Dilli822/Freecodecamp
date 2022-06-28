f9cb8d7911bb8d55dced74211e0537447ee0cb14
dilli <dillihangrae@gmail.com>
2022-06-28 22:27:38 +0545

    [added] adds javascript objects with freecodecademy..

---
+++ freecodecamp-JS/Javascript_Objects/js_objects.js
@@ -0,0 +1,15 @@
+const myDog = {
+    // Only change code below this line
+    name: "jackey",
+    legs: 4,
+    tails: 1,
+    friends: ["roose", "yutu"]
+    // Only change code above this line
+  };
+  
+  // javascript objects is same as array but having properties and methods with key value type pairs
+  const sample_Showing_Number_String = {
+    name: "this is string",
+    5: "number",
+    age: 23
+  }
