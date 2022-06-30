249412a6d4cbb83059089354e704671a3699d3e1
dilli <dillihangrae@gmail.com>
2022-06-30 09:31:31 +0545

    [added] adds accessing javascript object properties with dot and big bracket[] ....

---
+++ freecodecamp-JS/Accessing Object Properties with Bracket Notation/index.js
@@ -0,0 +1,13 @@
+
+// accessing object properties with Bracket Notation
+([])
+const myDetails = {
+    "userName": "dilli",
+    "age": 23,
+    "email": "dillihangrae@gmail.com",
+    "role": "student and developer"
+}
+
+console.log(myDetails["userName"]);
+console.log(myDetails["age"]);
+console.log(myDetails["role"]);
---
+++ freecodecamp-JS/Accessing Object Properties with Dot Notation/index.js
@@ -0,0 +1,15 @@
+// Accessing Object Properties with Dot Notation
+// Methods to access the properties of an object
+// with (.) and ([])
+
+const myDetails = {
+    prop1: "dilli ",
+    prop2: 23,
+    prop3: "dillihangrae@gmail.com"
+}
+
+const prop1val = myDetails.prop1();
+const prop2val = myDetails.prop2();
+
+console.log(prop1val);
+console.log(prop2val);
