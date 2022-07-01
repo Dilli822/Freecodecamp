0637237265d26e15255b932ad62d5852abeee96f
dilli <dillihangrae@gmail.com>
2022-07-01 12:27:21 +0545

    [added] adds access js object properties with barcket storing in variable..

---
+++ freecodecamp-JS/Accessing Object Properites with Bracket storing in variable/access properties with baracket storing in variable.js
@@ -0,0 +1,18 @@
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;
+const player = testObj[playerNumber];
+ // Change this line
+
+console.log(player);
+
+// accessing properties through variables
+const playerTwo = 12;
+const players = testObj[playerTwo];
+console.log(players);
