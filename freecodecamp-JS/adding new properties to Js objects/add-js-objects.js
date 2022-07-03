061c30a0e8a9f80da7bab8320f8cbbf51de878ae
dilli <dillihangrae@gmail.com>
2022-07-03 21:14:59 +0545

    [added] adds add js objects members to js objects..

---
+++ freecodecamp-JS/adding new properties to Js objects/add-js-objects.js
@@ -0,0 +1,10 @@
+// adding  objects members to object
+
+const myDetails = {
+  "name": "Dilli Hang Rai",
+  "age": 23,
+  "gender": "male",
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDetails.education = "bachelor-running";
