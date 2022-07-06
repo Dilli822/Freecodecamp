3c8925adac99bae20f6f2cebac6fb91de9d3fd64
dilli <dillihangrae@gmail.com>
2022-07-06 21:28:58 +0545

    [added] adds testing js objects existance by using lookups and decistion statement logics...

---
+++ freecodecamp-JS/Testing Objects Properties/testing.objects.properties.ts
@@ -0,0 +1,14 @@
+function checkObj(obj: {type: string}, checkProp:string) {
+  // Only change code below this line
+  // obj has properties of {gift: "pony", pet: "kitten", bed: "sleigh"}
+  // now obj is assigned to checkProp
+  // which will check checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "pet") returns Kitten
+  // checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "house") should return the string Not Found
+  if(obj.hasOwnProperty(checkProp)){
+    return obj[checkProp];
+  }
+  else{
+    return "Not Found";
+  }
+  // Only change code above this line
+}
