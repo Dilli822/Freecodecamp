553b46eb27b93437571f62691aa97f742796d424
dilli <dillihangrae@gmail.com>
2022-07-11 13:36:01 +0545

    [added] adds accessing nested arrays & objects...

---
+++ freecodecamp-JS/Accessing Nested Arrays & Objects/accessing-nested-obj-arry.js
@@ -0,0 +1,21 @@
+
+const myDetails = [
+    {
+        type: "favourites",
+        list: [
+            "color",
+            "song",
+            "music-video"
+        ]
+    },
+    {
+        type: "sports",
+        list: [
+            "football",
+            "basketball"
+        ]
+    }
+];
+
+const varFav = myDetails[1].list[0];
+console.log(varFav);
